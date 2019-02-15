3b2b6bfdbd297a9beb72a196c7b1b5c252fb7446
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2019-02-15 17:38:37 +0100

    Updated the theme

--- header.php
+++ header.php
@@ -1,29 +1,39 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<meta charset="utf-8">
-		<title></title>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-		<?php wp_head(); ?>
-	</head>
-	<body>
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		  <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon"></span>
-		  </button>
-		  <div class="collapse navbar-collapse" id="navbarNav">
-		    <ul class="navbar-nav">
-					<?php $pages = get_pages(); ?>
-					<?php foreach ($pages as $page ): ?>
-		      <li class="nav-item">
-		        <a class="nav-link" href="<?php echo get_permalink($page);?>">
-					 <?php echo $page->post_title; ?></a>
-		      </li>
-					<?php endforeach; ?>
-		    </ul>
-		  </div>
-	</nav>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-		<div class="container">
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+		<?php $args = [
+			'sort_column' => 'menu_order',
+			// 'numberposts' => 2,
+			// 'order' => 'desc',
+			] ?>
+
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul class="navbar-nav mr-auto">
+            <?php foreach (get_pages($args) as $page): ?>
+                <li class="nav-item <?php if (is_page($page)) { echo 'active'; } ?>">
+                    <a class="nav-link" href="<?php echo get_permalink($page); ?>">
+                        <?php echo $page->post_title; ?>
+                    </a><!-- /nav-link -->
+                </li><!-- /nav-item -->
+            <?php endforeach; ?>
+        </ul><!-- /navbar -->
+    </div><!-- /collapse -->
+  </nav>
+
+  <div class="container">
